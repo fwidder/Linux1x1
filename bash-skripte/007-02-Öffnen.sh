5e3a91108295522c8568e543b2e658f3decd548d
Florian Widder <florian.widder@live.de>
2019-11-17 19:53:04 +0100

    Add Die Bakn (Shell Skript) V1

---
+++ bash-skripte/007-02-Öffnen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+WRKDIR=$1
+KNR=$2
+NAME=$3
+SALDO=$4
+
+echo "Neues Konto für $NAME mit Kontonummer $KNR und folgendem Salso $SALDO € erstellen? (ja/nein)"
+read choise
+if [ "$choise" = "ja" ]
+then
+  pushd $WRKDIR
+  if test -f "$KNR"; then
+    echo "Konto $KNR existiert schon!"
+    exit 2
+  fi
+  echo $NAME > $KNR
+  echo $SALDO >> $KNR
+  echo Konto erstellt!
+  exit 0
+fi
+echo "Vorgang abgebrochen!"
---
+++ bash-skripte/007-05-Einzahlen.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+WRKDIR=$1
+KNR=$2
+BETRAG=$3
+
+pushd $WRKDIR
+if ! test -f "$KNR"; then
+    echo "Konto $KNR existiert nicht!"
+    exit 2
+fi
+
+NAME="$(cat $KNR | head -n1)"
+SALDO="$(cat $KNR | tail -n1)"
+
+echo "$BETRAG auf Konto $KNR von $NAME einzahlen? (ja/nein)"
+read choise
+if [ "$choise" = "ja" ]
+then
+  SALDO=$(($SALDO + $BETRAG))
+  echo $NAME > $KNR
+  echo $SALDO >> $KNR
+  echo Der neue Kontostand beträgt nun $SALDO!
+  exit 0
+fi
+echo "Vorgang abgebrochen!"
